18393fc7a3ffa8cdddbca64b6a488e93691d19db
AP Solanki <88495030+ateendra24@users.noreply.github.com>
2023-07-05 22:52:47 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function on(){
-    document.getElementById("learn_more").style.visibility = "visible";
     document.getElementById("learn_more").style.left = "50%";
+    document.getElementById("learn_more").style.display = "block";
 }
 
 function off(){
     document.getElementById("learn_more").style.left = "-50%";
-    document.getElementById("learn_more").style.visibility = "hidden";
+    document.getElementById("learn_more").style.display = "none";
 }
 
 
